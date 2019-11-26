d4b36060b0a9b9fdda27068a6a024673b744b18a
李小明 <394752324@qq.com>
2019-11-26 16:42:57 +0800

    库存价格

--- classes/CInventory.js
+++ classes/CInventory.js
@@ -1,24 +1,25 @@
 var SteamCommunity = require("../index.js");
 
-SteamCommunity.prototype.getInventoryItemPrice = function(
-	appid,
-	hashName,
-	currency,
-	country,
-	callback
-) {
-	var self = this;
-	this.httpRequest(
-		`https://steamcommunity.com/market/priceoverview/?country=${country}&currency=${currency}&appid=${appid}&market_hash_name=${encodeURIComponent(
+SteamCommunity.prototype.getInventoryItemPrice = function({ appid, hashName }) {
+	return new Promise((resolve, reject) => {
+		const url = `https://steamcommunity.com/market/priceoverview/?country=US&currency=1&appid=${appid}&market_hash_name=${encodeURIComponent(
 			hashName
-		)}`,
-		function(err, response, body) {
-			if (err) {
-				callback(err);
-				return;
-			}
-			callback(err, JSON.parse(body));
-		},
-		"steamcommunity"
-	);
+		)}`;
+		this.httpRequest(
+			url,
+			function(err, response, body) {
+				if (err) {
+					reject(err);
+				} else {
+					const resp = JSON.parse(body);
+					if (resp.success) {
+						resolve(resp);
+					} else {
+						reject(new Error(body));
+					}
+				}
+			},
+			"steamcommunity"
+		);
+	});
 };
